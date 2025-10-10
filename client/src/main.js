b8d8023f114ab4bfd3f5b0b6228d148d8c023afb
Oleksandra Malii <minelenova1@gmail.com>
2025-10-10 21:12:52 +0300

    feat: setup Login page

--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,7 @@
 import '../styles/global.css'
 import '../styles/variables.css'
 import '../styles/reset.css'
+import '../styles/inputs.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
