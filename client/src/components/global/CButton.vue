76db31fd58565191bed2fd49f9cbe7a7bbdccab8
Oleksandra Malii <minelenova1@gmail.com>
2025-10-12 04:07:24 +0300

    feat: add custom button component

---
+++ client/src/components/global/CButton.vue
@@ -0,0 +1,24 @@
+<template>
+    <button class="custom-button">
+        <slot></slot>
+    </button>
+</template>
+
+<style scoped>
+.custom-button {
+    display: inline-flex;
+    padding: 20px 40px;
+    justify-content: center;
+    align-items: center;
+    gap: 10px;
+    border-radius: 40px;
+    background: var(--color-violet);
+    color: var(--color-white);
+    font-weight: bold;
+    text-align: center;
+}
+.custom-button:hover {
+    background-color: var(--color-purple);
+    cursor: pointer;
+}
+</style>
