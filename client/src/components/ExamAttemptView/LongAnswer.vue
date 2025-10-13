483ff918348cac8422148148857cbe8823f8c4fa
Oleksandra Malii <minelenova1@gmail.com>
2025-10-13 18:21:26 +0300

    feat: design long answer question type

---
+++ client/src/components/ExamAttemptView/LongAnswer.vue
@@ -0,0 +1,53 @@
+<template>
+    <div class="question-block">
+        <textarea
+            class="textarea-input"
+            :value="modelValue"
+            @input="$emit('update:modelValue', $event.target.value)"
+            placeholder="Введіть вашу відповідь..."
+        ></textarea>
+    </div>
+</template>
+
+<script setup>
+defineProps({
+    modelValue: { 
+        type: String, 
+        default: '' 
+    }
+});
+
+defineEmits(['update:modelValue'])
+</script>
+
+<style scoped>
+.question-block {
+    width: 60%;
+    height: 400px; 
+    margin-bottom: 20px;
+}
+
+.textarea-input {
+    height: 100%; 
+    width: 100%;
+    padding: 20px;
+    background-color: var(--color-gray);
+    border: 3px solid var(--color-gray);
+    border-radius: 12px;
+    font-family: inherit;
+    font-size: inherit;
+    transition: all 150ms ease;
+    box-shadow: none;
+    resize: none;
+    line-height: 1.5; 
+}
+
+.textarea-input:hover {
+    border-color: var(--color-dark-gray);
+}
+
+.textarea-input:focus-visible {
+    outline: 3px solid var(--color-purple);
+    outline-offset: 2px; 
+}
+</style>
