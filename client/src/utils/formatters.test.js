6557628d51ed25f6857342dcf57135605cc6952c
Oleksandra Malii <minelenova1@gmail.com>
2025-10-21 20:24:08 +0300

    [RED] Add failing test for basic minute/second formatting

--- client/src/utils/formatters.test.js
+++ client/src/utils/formatters.test.js
@@ -1,2 +1,8 @@
 import { describe, it, expect } from 'vitest'
 import { formatDuration } from './formatters'
+
+describe('formatDuration', () => {
+  it('має правильно форматувати час менше години', () => {
+    expect(formatDuration(846)).toBe('14 хв 06 сек')
+  })
+})
