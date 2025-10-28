b803504dd123fd43b963f07699fd1506c11650be
Oleksandra Malii <minelenova1@gmail.com>
2025-10-28 23:37:35 +0200

    feat: add utility function for getting user role

--- src/utils/auth.py
+++ src/utils/auth.py
@@ -6,6 +6,8 @@ from src.core.config import JWT_SECRET, JWT_ALGORITHM
 from sqlalchemy.orm import Session
 from src.api.database import get_db
 from src.models.users import User
+from src.models.roles import Role
+from src.models.user_roles import UserRole
 
 security = HTTPBearer()
 
@@ -88,10 +90,26 @@ def get_current_user_id(credentials: HTTPAuthorizationCredentials = Depends(secu
             detail="Invalid authentication credentials"
         )
 
+def get_user_role(db: Session, user_id: UUID) -> str:
+    """
+    Знаходить та повертає назву ролі для вказаного користувача.
+    """
+    role_name = db.query(Role.name).join(UserRole).filter(UserRole.user_id == user_id).scalar()
+    
+    if not role_name:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="User has no assigned role. Access denied."
+        )
+        
+    return role_name
+
+
 def get_current_user(db: Session = Depends(get_db), user_id: UUID = Depends(get_current_user_id)) -> User:
     """
-    Залежність FastAPI, яка спочатку отримує ID користувача з токена,
-    а потім завантажує та повертає повний об'єкт користувача з бази даних.
+    Залежність FastAPI, яка отримує ID користувача з токена,
+    завантажує об'єкт користувача, перевіряє наявність ролі та додає
+    її як атрибут до об'єкта.
     """
     user = db.query(User).filter(User.id == user_id).first()
     if not user:
@@ -99,4 +117,5 @@ def get_current_user(db: Session = Depends(get_db), user_id: UUID = Depends(get_
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="User not found"
         )
+    user.role = get_user_role(db, user_id)
     return user
