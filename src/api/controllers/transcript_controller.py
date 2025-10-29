e5369697e6a866c92cc79560689961e4fa47645f
Oleksandra Malii <minelenova1@gmail.com>
2025-10-29 03:45:26 +0200

    fix: remove tests for an unimplemented function

--- src/api/controllers/transcript_controller.py
+++ src/api/controllers/transcript_controller.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from uuid import UUID
 from src.api.schemas.transcript import TranscriptResponse
 from src.api.services.transcript_service import TranscriptService
-from src.utils.auth import get_current_user
+from src.utils.auth import get_current_user_with_role
 from src.api.database import get_db
 from src.models.users import User
 
@@ -13,13 +13,14 @@ class TranscriptController:
         self.router = APIRouter(prefix="/transcript", tags=["Transcript"])
 
         @self.router.get("", response_model=TranscriptResponse, summary="Отримати атестат поточного користувача")
-        async def get_transcript( current_user: User = Depends(get_current_user), db: Session = Depends(get_db)):
+        async def get_transcript( current_user: User = Depends(get_current_user_with_role), db: Session = Depends(get_db)):
             """
             Повертає повну інформацію для сторінки "Мій атестат",
             включаючи список всіх курсів з найкращими оцінками та
             загальну статистику.
             """
             # Перевірка ролі
+
             if current_user.role != 'student':
                 raise HTTPException(
                     status_code=status.HTTP_403_FORBIDDEN, 
