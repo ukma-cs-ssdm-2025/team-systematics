2fa6752c02db07cba85189e962b951ee17f1fb66
Oleksandra Malii <minelenova1@gmail.com>
2025-11-04 19:25:38 +0200

    feat: reuse course component for different roles

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -8,7 +8,7 @@ import UnauthorizedView from '../views/UnauthorizedView.vue'
 import ExamResultsView from '../views/ExamResultsView.vue'
 import ExamReviewView from '../views/ExamReviewView.vue'
 import MyTranscriptView from '../views/MyTranscriptView.vue'
-import MyCoursesView from '../views/MyCoursesView.vue'
+import CoursesCatalogueView from '../views/CoursesCatalogueView.vue'
 import ExamJournalView from '../views/ExamJournalView.vue'
 import CourseExamsView from '../views/CourseExamsView.vue'
 import MyProfileView from '../views/MyProfileView.vue'
@@ -94,14 +94,24 @@ const router = createRouter({
       }
     },
     {
-      path: '/courses',
-      name: 'MyCourses',
-      component: MyCoursesView,
+      path: '/courses/my',
+      name: 'MyCoursesCatalogue',
+      component: CoursesCatalogueView,
       meta: {
         requiresAuth: true,
         requiresRole: 'teacher',
         title: 'Мої курси'
-      },
+      }
+    },
+    {
+      path: '/courses',
+      name: 'CoursesCatalogue',
+      component: CoursesCatalogueView,
+      meta: {
+        requiresAuth: true,
+        requiresRole: 'student',
+        title: 'Каталог курсів'
+      }
     },
     {
       path: '/courses/:courseId/exams',
@@ -132,7 +142,7 @@ const router = createRouter({
         title: 'Мій профіль'
       }
     },
-    ]
+  ]
 })
 
 // Перевіряє доступ до маршрутів перед переходом
@@ -149,13 +159,16 @@ router.beforeEach((to, from, next) => {
     if (to.meta.requiresRole === 'teacher' && auth.isTeacher.value) {
       hasAccess = true
     }
-    
+    if (to.meta.requiresRole === 'student' && auth.isStudent.value) {
+      hasAccess = true
+    }
+
     if (!hasAccess) {
       next({ path: '/forbidden' })
       return
     }
   }
-  next() 
+  next()
 })
 
 router.afterEach((to) => {
