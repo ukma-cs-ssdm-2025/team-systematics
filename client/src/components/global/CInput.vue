1b01b0505ff078f13256318bcd4bbd12930c90f6
Oleksandra Malii <minelenova1@gmail.com>
2025-11-06 18:28:51 +0200

    feat: add custom input component

---
+++ client/src/components/global/CInput.vue
@@ -0,0 +1,50 @@
+<template>
+  <input
+    class="custom-input"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    :disabled="disabled"
+  />
+</template>
+
+<script setup>
+defineProps({
+  modelValue: {
+    type: String,
+    default: ''
+  },
+    disabled: {
+    type: Boolean,
+    default: false
+  },
+})
+defineEmits(['update:modelValue'])
+</script>
+
+<style scoped>
+.custom-input {
+  width: 100%;
+  padding: 20px;
+  background-color: var(--color-gray);
+  border: 3px solid var(--color-gray);
+  border-radius: 12px;
+  font-family: inherit;
+  font-size: inherit;
+  transition: all 150ms ease;
+  box-shadow: none;
+  line-height: 1.5;
+}
+
+.custom-input:hover {
+  border-color: var(--color-dark-gray);
+}
+
+.custom-input:focus-visible {
+  outline: 3px solid var(--color-purple);
+  outline-offset: 2px;
+}
+
+.disabled {
+    cursor: not-allowed;
+}
+</style>
